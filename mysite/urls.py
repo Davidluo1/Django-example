380ea91aabf1f3f46f623842f6b48fe1d9713645
David Luo <62676858+Davidluo1@users.noreply.github.com>
2022-12-28 18:38:38 -0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls')),
+    path('admin/', admin.site.urls),
+    path('api/v1/user/', include('user.urls')),
+    path('api/v1/products/', include('products.urls')),
 ]
