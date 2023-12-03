fc93f5ca323573e2dfb567c65705acf026c4de85
David Luo <david.luo08@myhunter.cuny.edu>
2023-12-03 10:25:47 -0500

    Update user_deactivate_request.py

--- user/serializer/user_deactivate_request.py
+++ user/serializer/user_deactivate_request.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 
-
 class UserDeactivateRequest(serializers.Serializer):
     """"User deactivate input class"""
-    
     email = serializers.CharField(max_length=50)
     password = serializers.CharField(max_length=50)
     otp = serializers.CharField(max_length=50)
