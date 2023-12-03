51af95f32cceab8888da6c55a07aee4ea3d75acd
David Luo <david.luo08@myhunter.cuny.edu>
2023-12-02 19:04:32 -0500

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from user.views import UserSignUpView, UserDeactivateView, UserInfo, UserLoginView, User_OtpVerifyView, AddressView, AddressDefaultView
+
 urlpatterns = [
     path('signup', UserSignUpView.as_view()),
     path('activate', User_OtpVerifyView.as_view()),
